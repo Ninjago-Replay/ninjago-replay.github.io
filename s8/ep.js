4c59a8201d3cd9476cf3b68c008cbcf1f1a402e5
Ninjago-Replay <ninjago.replay.official@gmail.com>
2024-08-07 09:21:55 +0100

    2024.8.7

---
+++ s8/ep.js
@@ -0,0 +1,51 @@
+// Define an array of episodes with title and photo URL
+const episodes = [
+    { title: 'الحلقة 1 : قناع الخداع', pageUrl: 'ep1.html' },
+    { title: 'الحلقة 2 : أميرة الحجر الكريم', pageUrl: 'ep2.html' },
+    { title: 'الحلقة 3 : الأوني والتنين', pageUrl: 'ep3.html' },
+    { title: 'الحلقة 4 : ثعبان جاكوار', pageUrl: 'ep4.html' },
+    { title: 'الحلقة 5 : صرخة رجل ميت', pageUrl: 'ep5.html' },
+    { title: 'الحلقة 6 : الرجل الصامت', pageUrl: 'ep6.html' },
+    { title: 'الحلقة 7 : لعبة الأقنعة', pageUrl: 'ep7.html' },
+    { title: 'الحلقة 8 : الخوف عند الوصول', pageUrl: 'ep8.html' },
+    { title: 'الحلقة 9 : إمكانية حقيقية', pageUrl: 'ep9.html' },
+    { title: 'الحلقة 10 : مشكلة كبيرة في نينجاجو الصغير', pageUrl: 'ep10.html' }
+];
+
+// Function to create the episode list
+function createEpisodeList() {
+    const episodeList = document.getElementById('episode-list');
+
+    episodes.forEach((episode) => {
+        const listItem = document.createElement('li');
+        listItem.className = 'episode-item';
+        listItem.innerHTML = `<a href="${episode.pageUrl}">${episode.title}</a>`;
+        episodeList.appendChild(listItem);
+    });
+}
+
+// Function to toggle the episode list visibility
+function toggleEpisodeList() {
+    const episodeList = document.getElementById('episode-list');
+    const toggleButton = document.getElementById('toggle-button');
+    if (episodeList.style.display === 'none') {
+        episodeList.style.display = 'block';
+        toggleButton.textContent = 'إخفاء القائمة';
+    } else {
+        episodeList.style.display = 'none';
+        toggleButton.textContent = 'عرض قائمة';
+    }
+}
+
+// Add event listener to the button
+function setupToggleButton() {
+    const toggleButton = document.getElementById('toggle-button');
+    toggleButton.addEventListener('click', toggleEpisodeList);
+}
+
+// Add event listener to DOMContentLoaded event to ensure the script runs after the HTML content is loaded
+document.addEventListener('DOMContentLoaded', function() {
+    createEpisodeList();
+    setupToggleButton();
+});
+
